ff28fd90c2fe918f9303bb885d4c746832502192
laukho <lkhouri92@gmail.com>
2020-01-06 15:50:52 +0100

    router

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
@@ -21,9 +22,70 @@ class HomepageController extends AbstractController
 	/**
 	 * @Route("/", name="homepage.index")
 	 */
-	public function index():Response
+	public function index(Request $request):Response
 	{
+		/*
+		 * débogage avec le composant debug
+		 *  dump : affichage dans le barre de débogage
+		 *  dd : dump and die
+		*/
+		//dd($request->server->get('HTTP_USER_AGENT'));
+
 		// retour de la réponse HTTP
-		return new Response('coucou');
+		/*return new Response('{ "data" : [ { "content" : "value" } ] }', Response::HTTP_FORBIDDEN, [
+			'Content-Type' => 'application/json'
+		]);*/
+
+		/*
+		 * retour d'une vue twig
+		 *   les vues sont à stocker dans le dossier templates
+		 *   à la racine du dossier, sont stockées les pages parentes
+		 *   dans les dossiers, sont stockées les pages enfants
+		 * architecture recommandée
+		 *   dans le dossier templates :
+		 *      - créer un dossier du même nom que le contrôleur
+		 *      - la vue reprend le nom de la méthode
+		 *   render cible automatiquement le dossier templates
+		 */
+
+		$userAgent = $request->server->get('HTTP_USER_AGENT');
+
+		/*
+		 * pour envoyer des informations à la vue, utiliser le second paramètre de render sous forme de tableau associatif
+		 * dans la vue, c'est la clé qui est récupérée
+		 */
+		return $this->render('homepage/index.html.twig', [
+			'param' => $userAgent
+		]);
+
+
+	}
+
+	/*
+	 * paramètre dans la route
+	 *    dans le schéma de la route, utiliser des accolades
+	 *       en PHP pur : page.php?firstname=tutu
+	 *    les paramètres de la route se retrouvent obligatoirement en paramètres de la méthode
+	 *
+	 * méthodes HTTP acceptées par la route : paramètre methods
+	 */
+
+	/**
+	 * @Route("/hello/{firstname}-{lastname}", name="homepage.hello", methods={"GET", "PUT"})
+	 */
+	public function hello(?string $firstname = null, ?string $lastname = null):Response
+	{
+		return $this->render('homepage/hello.html.twig', [
+			'firstname' => $firstname,
+			'lastname' => $lastname
+		]);
 	}
 }
+
+
+
+
+
+
+
+
