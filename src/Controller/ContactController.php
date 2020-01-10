2b1ec89a633f2eaaef6a19ee9338631f427d9310
laukho <lkhouri92@gmail.com>
2020-01-10 14:48:36 +0100

    contact form complete

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use App\Form\Model\ContactFormModel;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,7 +17,7 @@ class ContactController extends AbstractController
 	/**
 	 * @Route("/contact", name="contact.index")
 	 */
-	public function index(Request $request):Response
+	public function index(Request $request, MailerInterface $mailer):Response
 	{
 		/*
 		 * affichage d'un formulaire
@@ -28,6 +31,54 @@ class ContactController extends AbstractController
 		$model = new ContactFormModel();
 		$form = $this->createForm($type, $model);
 		$form->handleRequest($request);
+
+		// si le formulaire est valide
+		if($form->isSubmitted() && $form->isValid()){
+			//dd($model);
+			/*
+			 * envoi d'un email
+			 *   service d'envoi d'email dans symfony : MailerInterface
+			 * types d'email:
+			 *   Email: permet d'écrire le contenu de l'email directement dans la classe
+			 *   TemplatedEmail: permet d'écrire le contenu de l'email dans des templates twig
+			 */
+			$message = (new TemplatedEmail())
+				->from( $model->getEmail() ) // expéditeur
+				->to(' 4fac0ec5a7-6da8bd@inbox.mailtrap.io') // destinataire
+				->subject('Contact') // sujet de l'email
+				->textTemplate('emailing/contact.txt.twig') // cibler un template twig au format txt
+				->context([
+					'firstname' => $model->getFirstname(),
+					'lastname' => $model->getLastname(),
+					'message' => $model->getMessage(),
+				]) // permet d'envoyer des variables au template
+			;
+
+			/*$message = (new Email())
+				->from($model->getEmail()) // expéditeur
+				->to(' 4fac0ec5a7-6da8bd@inbox.mailtrap.io') // destinataire
+				->subject('Contact') // sujet de l'email
+				->text("
+					********** nom prénom ********** 
+					{$model->getFirstname()} {$model->getLastname()}
+					********** message ********** 
+					{$model->getMessage()}
+				")
+				->html("
+					<h3 style='color: tomato'>
+						{$model->getFirstname()} {$model->getLastname()}
+					</h3>
+					<p>{$model->getMessage()}</p>
+				")
+			;*/
+			$mailer->send($message);
+
+			// message flash : message stocké en session et affiché une seule fois
+			$this->addFlash('notice', "Votre message a été envoyé");
+
+			// redirection vers une route par son nom
+			return $this->redirectToRoute('homepage.index');
+		}
 
 		return $this->render('contact/index.html.twig', [
 			'form' => $form->createView()
